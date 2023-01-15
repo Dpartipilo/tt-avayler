4139eeb8f2fe4eea8dd70ca4f55adc093529ccdc
Dpartipilo <Dpartipilo@hotmail.com>
2023-01-15 09:36:07 +0000

    Implmenented initial components layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
+import { AppProvider } from '../src/Providers/AppProvider';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <>
+        <Component {...pageProps} />;
+      </>
+    </AppProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/Providers/AppProvider.tsx
@@ -0,0 +1,66 @@
+import { createContext, ReactElement, useCallback, useMemo, useState } from 'react';
+import * as TYPES from '../types';
+import axios from 'axios';
+
+export const baseURL = 'https://api.spacexdata.com/v5/launches';
+
+type TContext = {
+  isLoading: boolean;
+  launches: TYPES.LaunchesProps[];
+  // eslint-disable-next-line no-unused-vars
+  getLaunches: (limit?: number) => void;
+};
+
+export const AppContext = createContext<TContext>({
+  isLoading: false,
+  launches: [],
+  getLaunches: () => null,
+});
+
+type AppProviderProps = { children?: ReactElement };
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [serverError, setServerError] = useState<unknown>(null);
+  const [launches, setLaunches] = useState<TYPES.LaunchesProps[]>([]);
+
+  const makeRequest = useCallback(async (method: string, url: string, body?: {}) => {
+    setIsLoading(true);
+
+    try {
+      const resp = await axios({
+        method,
+        url,
+        data: body,
+      });
+
+      const data = await resp?.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+      setServerError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const getLaunches = useCallback(
+    async (limit: number) => {
+      const launches = await makeRequest('post', `${baseURL}/query`, { options: { limit } });
+      console.log(launches);
+      setLaunches(launches.docs);
+    },
+    [makeRequest]
+  );
+
+  const providerValue = useMemo(() => {
+    return {
+      isLoading,
+      launches,
+      serverError,
+      getLaunches,
+    };
+  }, [isLoading, launches, serverError, getLaunches]);
+
+  return <AppContext.Provider value={providerValue}>{children}</AppContext.Provider>;
+};
